167410efefa731e219739ef5ad56d8eeee0a4cf2
Roman Timonin <rtimonin569@gmail.com>
2024-06-08 00:16:29 +0300

    login

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -17,3 +18,14 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():  # запускается проверка данных из формы
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))  # flash, сообщение для пользователя при успешной отправке
+        # данных формы
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
