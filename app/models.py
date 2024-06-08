36913efc1ae8c58b1803c6f13bf50a3c6526b948
Roman Timonin <rtimonin569@gmail.com>
2024-06-08 03:11:38 +0300

    Flask-Login

--- app/models.py
+++ app/models.py
@@ -1,11 +1,15 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from flask_login import UserMixin
+from app import login
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
+    """Модель таблицы БД пользователей."""
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
@@ -16,8 +20,23 @@ class User(db.Model):
     def __repr__(self):
         return '<User> {}'.format(self.username)
 
+    def set_password(self, password):
+        """Метод генерирует хэш при создании пароля."""
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        """Метод проверяет, совпадает ли хэш сгенерированный ранее."""
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    """Метод загрузки пользователя из БД с заданным идентификатором."""
+    return db.session.get(User, int(id))
+
 
 class Post(db.Model):
+    """Модель таблицы БД записей (постов)."""
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     body: so.Mapped[str] = so.mapped_column(sa.String(140))
     timestamp: so.Mapped[datetime] = so.mapped_column(
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,16 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user
+import sqlalchemy as sa
 from app import app
+from app import db
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    """Функция представления главной страницы."""
+    """Метод представления главной страницы."""
     user = {'username': 'Roman'}
     posts = [
         {
@@ -23,11 +27,18 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    """Функция представления формы авторизации."""
+    """Метод представления формы авторизации."""
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
-    if form.validate_on_submit():  # запускается проверка данных из формы
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))  # flash, сообщение для пользователя при успешной отправке
-        # данных формы
+    if form.validate_on_submit():  # validate_on_submit - проверяет и собирает данные из формы
+        user = db.session.scalar(sa.select(User).where(
+            User.username == form.username.data))  # scalar - возвращает первое значение первой строки результата
+        if user is None or not user.check_password(form.password.data):  # проверка, что пароль действителен либо нет
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)  # функция регистрирует пользователя как вошедшего в
+        # систему, а это означает, что для всех будущих страниц, на которые пользователь перейдет, будет задана
+        # переменная current_user для этого пользователя
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
